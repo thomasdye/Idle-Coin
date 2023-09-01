ad429194efb9ef481a7c18eed2dcad1d7c98354d
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-09-01 03:49:16 -0400

    added second bar. removed buttons for now

--- game.js
+++ game.js
@@ -1,63 +1,43 @@
+const coinDisplay = document.getElementById("coinCount");
+const copperBar = document.getElementById("copperBar");
+const silverBar = document.getElementById("silverBar");
+
 let coins = 0;
-let upgradeCost = 10;
-let fillRate = 1; // % per interval
-let progress = 0; // % of the progress bar
-let isFilling = true;
+let copperProgress = 0;
+let silverProgress = 0;
+const fillRate = 1;
+let isCopperFilling = true;
+let isSilverFilling = false;
 
-// Element references
-const coinCountElement = document.getElementById('coinCount');
-const progressBar = document.getElementById('progressBar');
-const upgradeButton = document.getElementById('upgradeButton');
-
-// Update display
 function updateDisplay() {
-    coinCountElement.textContent = coins;
-    progressBar.style.width = progress + '%';
-    upgradeButton.disabled = coins < upgradeCost;
+    coinDisplay.textContent = "Coins: " + coins;
+    if (coins >= 50 && !isSilverFilling) {
+        isSilverFilling = true;
+        // Once unlocked, don't check again
+        coins -= 50; // Spend 50 coins to unlock
+    }
 }
 
-// Fill progress bar
 function fillProgressBar() {
-    if (!isFilling) return;  // If it's not filling, just return without doing anything
-
-    progress += fillRate;
-
-    if (progress >= 100) {
-        // Set isFilling to false to stop updating progress and coins during the transition
-        progressBar.style.width = '100%'; // Ensure the bar is visually full
-        isFilling = false;
-
-        // Fade out progress bar
-        progressBar.style.opacity = 0;
-        
-        setTimeout(() => {
+    if (isCopperFilling) {
+        copperProgress += fillRate;
+        if (copperProgress >= 100) {
+            copperProgress = 0;
             coins++;
-            progress = 0; // =\eset progress bar width
-            progressBar.style.width = progress + '%';
-            updateDisplay();
-
-            // Wait a bit after the fade-out, then start refilling
-            setTimeout(() => {
-                progressBar.style.opacity = 1;
-                // Set isFilling back to true after the transition so the bar can start refilling
-                isFilling = true;
-            }, 200); // 200ms delay after the fade-out
-
-        }, 300); // match the duration of the opacity transition in CSS
-    } else {
-        progressBar.style.width = progress + '%';
+        }
+        copperBar.style.width = copperProgress + '%';
     }
+
+    if (isSilverFilling) {
+        silverProgress += fillRate / 5; // To make silver 5 times slower
+        if (silverProgress >= 100) {
+            silverProgress = 0;
+            coins += 5;
+        }
+        silverBar.style.width = silverProgress + '%';
+    }
+
+    updateDisplay();
 }
 
-// Upgrade action
-upgradeButton.addEventListener('click', function() {
-    if (coins >= upgradeCost) {
-        coins -= upgradeCost;
-        fillRate *= 1.5; // increase the fill rate by 50%
-        updateDisplay();
-    }
-});
-
-setInterval(fillProgressBar, 100); // update every 100ms
-
-updateDisplay(); // Initial display update
+setInterval(fillProgressBar, 100);
